5389a5f1127d3c1ffefadc4c30737930e04a4d18
madhurn29 <madhur.nadamwar29@gmail.com>
2022-09-04 21:35:01 +0530

    Logical Operator

---
+++ Logical-Operators/problem3.js
@@ -0,0 +1,9 @@
+let char = "p";
+
+if(char!="a" && char!="e" && char!="i" && char!="o" && char!="u")
+{
+  console.log("consonant")
+}
+else {
+  console.log("vowel")
+}
---
+++ Logical-Operators/problem4.js
@@ -0,0 +1,15 @@
+// Given 3 numbers (all different values), print which is greatest
+
+let a=18 ,b=15, c=22;
+
+if(a>b && a>c)
+{
+  console.log(a);
+}
+else if(b>a &&b>c)
+{
+  console.log(b)
+}
+else{
+  console.log(c);
+}
